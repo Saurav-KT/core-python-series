c2bc0b655b83b30105c4fd705afd7fc4d01b03f7
saurav j <saurav.saurav12@gmail.com>
2025-03-12 23:47:14 +0530

    Refactor code-base

--- abstract_class_example.py
+++ abstract_class_example.py
@@ -1,25 +1,41 @@
 from abc import ABC,abstractmethod
 
-class Animal(ABC):
+class Shape(ABC):
 
     @abstractmethod
-    def makesound(self):
+    def area(self):
         pass
 
-    def sleep(self):
-        print("sleeping")
+    def perimeter(self):
+        # Method to calculate the area
+        pass
 
-class Dog(Animal):
-    def makesound(self):
-        print("Woof")
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
 
-class Cat(Animal):
-    def makesound(self):
-        print("Meow")
+    def area(self):
+        return 3.14 * self.radius ** 2
 
-dog= Dog()
-dog.makesound()
-dog.sleep()
+    def perimeter(self):
+        return 2 * 3.14 * self.radius
 
-cat= Cat()
-cat.makesound()
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width= width
+        self.height= height
+
+    def area(self):
+        return self.width* self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+# Example Usage
+circle = Circle(5)
+print("Circle Area:", circle.area())          # Output: 78.5
+print("Circle Perimeter:", circle.perimeter())  # Output: 31.4
+
+rectangle = Rectangle(4, 6)
+print("Rectangle Area:", rectangle.area())          # Output: 24
+print("Rectangle Perimeter:", rectangle.perimeter())  # Output: 20
